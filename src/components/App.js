0d1fb84febb682cc930314b33250a007857446f1
Yisak Girmay <yisakbruck23@gmail.com>
2023-06-19 14:16:05 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,19 @@
+// import React from "react";
+// import { Route, Switch } from "react-router-dom";
+// import NavBar from "./NavBar";
+// import Home from "./Home";
+// import Actors from "./Actors";
+// import Directors from "./Directors";
+// import Movies from "./Movies";
+
+// function App() {
+//   return <div>{/*{code here}*/}</div>;
+// }
+
+// export default App;
+
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { NavLink, Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
@@ -7,7 +21,17 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/movies" component={Movies} />
+        <Route path="/directors" component={Directors} />
+        <Route path="/actors" component={Actors} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,26 @@
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+
+// function NavBar() {
+//   return <div>{/*{code here}*/}</div>;
+// }
+
+// export default NavBar;
+
+
 import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/movies">Movies</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
+
